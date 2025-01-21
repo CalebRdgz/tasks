78cb4aebc3b720c87eaefa513e882b721fc8cf97
Caleb Rodriguez <caleb.rodriguez@aa.com>
2025-01-21 16:16:06 -0600

    first commit

---
+++ src/test/java/com/caleb/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caleb.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
