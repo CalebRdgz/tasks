1882e18db4c5c023bbbd330306b7bee175fc0a12
Caleb Rodriguez <calebrodgz@gmail.com>
2025-02-11 15:57:44 -0600

    setup new spring boot project and configured a postgreSQL database using docker-compose.yml

--- src/main/java/com/caleb/tasks/TasksApplication.java
+++ src/main/java/com/caleb/tasks/TasksApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TasksApplication {
-
+	//Main method runs the Spring Boot application:
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
--- src/test/java/com/caleb/tasks/TasksApplicationTests.java
+++ src/test/java/com/caleb/tasks/TasksApplicationTests.java
@@ -3,7 +3,7 @@ package com.caleb.tasks;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest //@SpringBootTest annotation starts up a test version of the application to run a test against it
 class TasksApplicationTests {
 
 	@Test
